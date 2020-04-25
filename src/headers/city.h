ff8ddaad150ebf4c1e02178de91e016531d2598b
SergioMachi <sergio.machi@srax.com>
2020-04-24 21:37:57 -0700

    Translated src/headers/city.h

--- src/headers/city.h
+++ src/headers/city.h
@@ -3,17 +3,23 @@
 #include <ncurses.h>
 
 typedef enum{
-	FUEGO, AGUA, PASTIZAL, EDIFICIO
-} Tipo_terreno;
+	FIRE, WATER, PASTURE, BUILDING
+} Ground_type;
 
-typedef struct Terreno{
-	Tipo_terreno tipo;
+typedef struct Ground{
+	Ground_type tipo;
 	bool dinamitado;
 	bool enPeligro;
-} Terreno;
+} Ground;
 
-void prepararCiudad(Terreno ciudad[][10], int nivel, int x, int y);
-void setPeligro(Terreno ciudad[][10], int nivel);
-void incendiar(Terreno ciudad[][10], int nivel);
-int numIncendios(Terreno ciudad[][10], int nivel);
-void setPeligroMult(int x, int y, int nivel, Terreno ciudad[][10]);
+/* void prepararCiudad(Ground ciudad[][10], int nivel, int x, int y);
+void setPeligro(Ground ciudad[][10], int nivel);
+void incendiar(Ground ciudad[][10], int nivel);
+int numIncendios(Ground ciudad[][10], int nivel);
+void setPeligroMult(int x, int y, int nivel, Ground ciudad[][10]); */
+
+void buildCity(Ground city[][10], int level, int x, int y);
+void setDanger(Ground city[][10], int level);
+void burnDown(Ground city[][10], int level);
+int getNumFires(Ground city[][10], int level);
+void setDangerMult(int x, int y, int level, Ground city[][10]);
