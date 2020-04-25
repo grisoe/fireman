edad4c35197ad05d19647901bf67e925be8d66b9
SergioMachi <sergio.machi@srax.com>
2020-04-25 00:33:10 -0700

    Translated src/player.c

--- src/player.c
+++ src/player.c
@@ -2,69 +2,69 @@
 
 #include "headers/player.h"
 
-void prepararJugador(Jugador *j){
+void setPlayer(Player *j){
 
-	//Tres variables que representan atributos de todo jugador.
+	//Tres variables que representan atributos de todo Player.
 	//Se podrían almacenar los valores ingresados por el usuario directamente
-	//en los atributos del jugador, pero cero que es mejor así por cuestiones
+	//en los atributos del Player, pero cero que es mejor así por cuestiones
 	//de validación: ya que sean valores validados, se guardan allá.
-	char nombre[50] = "";
-	int nivel;
-	int turnos;
+	char name[50] = "";
+	int level;
+	int turns;
 
-	//Nombre sin validar al estilo arcade ("Anonimo" si no ingresa nombre).
+	//name sin validar al estilo arcade ("Anonimo" si no ingresa name).
 	//"Anonimo", así, sin acento es por por necesidad más que por ignorancia
-	//o pereza: al momento de querer hacer consultas por nombre, deberías
+	//o pereza: al momento de querer hacer consultas por name, deberías
 	//ingresarlo con acento, pero ncurses muestra basura cuando quieres hacer eso.
 	//Algún día lo solucionaré.
-	printw("Ingresa tu nombre: ");
-	scanw("%[^\n]", nombre);
+	printw("Ingresa tu name: ");
+	scanw("%[^\n]", name);
 
-	if(strlen(nombre) == 0){
-		strcpy(j->nombre, "Anonimo");
+	if(strlen(name) == 0){
+		strcpy(j->name, "Anonimo");
 	}else{
-		strcpy(j->nombre, nombre);
+		strcpy(j->name, name);
 	}
 
-	printw("Ingresa el nivel de dificultad (3-10): ");
-	scanw("%d", &nivel);
-	while(nivel < 3 || nivel > 10){
+	printw("Ingresa el level de dificultad (3-10): ");
+	scanw("%d", &level);
+	while(level < 3 || level > 10){
 
-		printw("\nLos niveles de dificultad están entre 3 y 10.\n\n");
+		printw("\nLos leveles de dificultad están entre 3 y 10.\n\n");
 
-		printw("Ingresa el nivel de dificultad (3-10): ");
-		scanw("%d", &nivel);
-
-	}
-	j->nivel = nivel;
-
-	printw("Ingresa el número de turnos (5-30): ");
-	scanw("%d", &turnos);
-	while(turnos < 5 || turnos > 30){
-
-		printw("\nEl número de turnos está entre 5 y 30.\n\n");
-
-		printw("Ingresa el número de turnos (5-30): ");
-		scanw("%d", &turnos);
+		printw("Ingresa el level de dificultad (3-10): ");
+		scanw("%d", &level);
 
 	}
-	j->turnos = turnos;
+	j->level = level;
+
+	printw("Ingresa el número de turns (5-30): ");
+	scanw("%d", &turns);
+	while(turns < 5 || turns > 30){
+
+		printw("\nEl número de turns está entre 5 y 30.\n\n");
+
+		printw("Ingresa el número de turns (5-30): ");
+		scanw("%d", &turns);
+
+	}
+	j->turns = turns;
 
 }
 
-//Se cuentan los puntos en función de los edificios en pie y se asignan al jugador.
-void setPuntuacion(Jugador *j, Terreno ciudad[][10]){
+//Se cuentan los puntos en función de los edificios en pie y se asignan al Player.
+void setPonints(Player *j, Ground city[][10]){
 
 	int i;
 	int k;
 
-	j->puntuacion = 0;
+	j->points = 0;
 
-	for(i = 0; i < j->nivel; i++){
-		for(k = 0; k < j->nivel; k++){
+	for(i = 0; i < j->level; i++){
+		for(k = 0; k < j->level; k++){
 			
-			if(ciudad[i][k].tipo == EDIFICIO){
-				(j->puntuacion)+=100;				
+			if(city[i][k].type == BUILDING){
+				(j->points)+=100;				
 			}
 
 		}
